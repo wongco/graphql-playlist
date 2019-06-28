3ff838a97d3702d37c6610f3b6713cbb4e1214f0
Ginson Wong <ginson.wong@hingehealth.com>
2019-06-28 16:24:32 -0700

    add author root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 // this file describes the schema for the graph data
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLInt } = graphql;
 
 // example data
 const books = [
@@ -9,12 +9,27 @@ const books = [
   { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
 ];
 
+const authors = [
+  {name: 'Patrick Rothfuss', age: 44, id: "1"},
+  {name: 'Brandon Sanderson', age: 42, id: "2"},
+  {name: 'Terry Pratchett', age: 66, id: "3"},
+]
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -22,12 +37,20 @@ const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     book: {
-      // boot root query, info below on how to deal request
+      // book root query, info below on how to deal request
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
         return books.find(book => book.id === args.id);
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return authors.find(author => author.id === args.id);
       }
     }
   }
