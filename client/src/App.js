5789ffb69bd1848b3fb930b5ebcc8eb402cac671
wongco <ginson@gmail.com>
2019-07-06 22:58:21 -0700

    add apollo and connect to application

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost"; // contains apollo client
+import { ApolloProvider } from "react-apollo";
 
-function App() {
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const App = () => {
   return (
-    <div id="main">
-      <h1>WongCo's Reading List</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>WongCo's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
