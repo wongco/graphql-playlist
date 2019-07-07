7f2431d44f7cb7a2da3363c96abf2bcbe5e79991
wongco <ginson@gmail.com>
2019-07-06 19:23:32 -0700

    add mongodb and add env vars

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoDbUser = process.env.DB_USER;
+const mongoDbPass = process.env.DB_PASS;
+const mongoDbSrv = process.env.DB_SRV;
+
+mongoose.connect(
+  `mongodb+srv://${mongoDbUser}:${mongoDbPass}@${mongoDbSrv}/test?retryWrites=true&w=majority`,
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected");
+});
 
 app.use(
   "/graphql",
